a847cfb0c7a63dea36b026421a3658d45462e25f
yuriBaza23 <yuribaza2321@gmail.com>
2021-04-28 02:36:30 -0300

    tests

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,25 @@
-export function Content() {
-  // Complete aqui
+import { MovieProps } from "../App";
+import { MovieCard } from "./MovieCard";
+
+interface ContentProps {
+  category: string;
+  movies: MovieProps[]
+}
+
+export function Content({ category, movies }: ContentProps) {
+  return(
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {category}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie:any) => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
